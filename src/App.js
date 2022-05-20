30030cf08b43404d13f2dc2cc8b39e780032dee1
aaronkg1 <aaronkg1@gmail.com>
2022-05-20 19:06:57 +0100

    Styling changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Link from "react-router-dom";
 
 import "./styles/App.css";
 function App() {
-  return <div className="home-container"></div>;
+  return (
+    <div className="home-container">
+      <div>
+        <h1></h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
